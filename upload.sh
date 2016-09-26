3dae90603eca696d536a3485dfb8a980ffa49368
dyrobin <yang.r.deng@gmail.com>
2016-09-26 09:39:01 +0300

    * Add ignore file * Improve upload.sh   - only upload newer file than last uploading   - an option to dry-run

--- upload.sh
+++ upload.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 # This script is used to upload changed files to corresponding directory in the server. 
-# Changed files are determined by 'git-diff' with different options.
+# Changed files are determined by 'git-diff' with different modes.
 #
+# If changed file has ever been uploaded and there is no modification since then,
+# it will be ignored when next uploading. The timestamp of uploading is stored as
+# .timestamp file and if it is missing, all changed files will be uploaded.
+# 
 # Note: This script requires kanssani configuration at ~/.ssh/config
+
+SCRIPT_PATH="$(cd $(dirname $0) && pwd)"
+TIMESTAMP="$SCRIPT_PATH/.timestamp"
+ERROR_LOG="error.log"
+
 
 SERVER="kanssani"
 CUSTOM_PATH="public_html/store/wp-content/plugins/theme-customisations-master"
 THEME_PATH="public_html/store/wp-content/themes/stationery"
 
+
 SCP="scp -q"
-ERROR="error.log"
 
 # remove error log
-rm -f $ERROR
+rm -f $ERROR_LOG
 
 # make sure at 'kanssani' branch 
 current_branch=`git rev-parse --abbrev-ref HEAD`
@@ -22,55 +31,81 @@ if [[ $current_branch != "kanssani" ]]; then
     exit 1
 fi
 
-# get changed files according to 'git-diff' option
-if [[ $# -eq 0 ]]; then
-    files=`git diff --name-only`
-else
-    case $1 in
-        "cached")
-            files=`git diff --cached --name-only`
+# get options
+while getopts ":dm:" opt; do
+    case $opt in
+        d)
+            dry_run=1
             ;;
-        "commit")
-            files=`git diff --name-only HEAD~1 HEAD`
+        m)
+            mode=$OPTARG
             ;;
         *)
-            echo "Usage: sh $0 [cached|commit]"
+            echo "Usage: sh $0 [-d] [-m index|cached|commit]"
             exit 1
     esac
+done
+
+if [[ -z $mode ]]; then
+    mode="index"
 fi
 
+# get changed files according to mode
+case $mode in
+    "index")
+        files=`git diff --name-only`
+        ;;
+    "cached")
+        files=`git diff --cached --name-only`
+        ;;
+    "commit")
+        files=`git diff --name-only HEAD~1 HEAD`
+        ;;
+    *)
+        echo "Usage: sh $0 [-d] [-m cached|commit]"
+        exit 1
+esac
+
+# upload
+for file in $files; do
+    if [[ $file =~ ^custom/ ]]; then
+        remote_dir=$CUSTOM_PATH/`dirname $file`
+    elif [[ $file =~ ^theme-template/ ]]; then
+        remote_dir=$THEME_PATH/`dirname ${file#theme-template/}`
+    else
+        continue
+    fi
+
+    # only upload newer file
+    if [[ $file -nt $TIMESTAMP ]]; then
+        printf "$file... "
+        cmd="ssh $SERVER mkdir -p $remote_dir && $SCP $file $SERVER:$remote_dir"
+
+        if [[ $dry_run -eq 1 ]]; then
+            echo ""
+            echo $cmd
+        else
+            sh -c "$cmd" 2>&1 >> $ERROR_LOG
+            if [[ $? -ne 0 ]]; then
+                echo "Err."
+                error=1
+            else
+                echo "OK."
+            fi
+        fi
+    fi
+done
+
 # exit if no changed files
-if [[ -z $files ]]; then
+if [[ -z $cmd ]]; then
     echo "Nothing needs to upload."
     exit 0
 fi
 
-# upload
-for file in $files; do
-    cmd=
-    if [[ $file =~ ^custom/ ]]; then
-        dir_name=$CUSTOM_PATH/`dirname $file`
-        cmd="ssh $SERVER mkdir -p $dir_name && $SCP $file $SERVER:$dir_name"
-    elif [[ $file =~ ^theme-template/ ]]; then
-        cmd="$SCP $file $SERVER:$THEME_PATH"
-    fi
-
-    printf "$file... "
-    if [[ -n $cmd ]]; then
-        sh -c "$cmd" 2>> $ERROR
-        if [[ $? -ne 0 ]]; then
-            echo "Err."
-        else
-            echo "OK."
-        fi
-    else
-        echo "No."
-    fi
-done
-
-if [[ -f $ERROR ]]; then
+# update timestamp if no errors
+if [[ -z $error ]]; then
+    touch $TIMESTAMP
+elif [[ -f $ERROR_LOG ]]; then
     echo ""
-    echo "Read $ERROR to see more details."
+    echo "Read $ERROR_LOG to see more details."
 fi
-
-
